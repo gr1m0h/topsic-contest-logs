a21b2140923d40505678b7f156a4704a4d07ec42
grimoh <dev@grimoh.net>
2022-09-23 16:58:27 +0900

    Update sql-2/contest002-3.sql

--- sql-2/contest002-3.sql
+++ sql-2/contest002-3.sql
@@ -1,5 +1,7 @@
-select p.PF_CODE as CODE, p.PF_NAME as NAME, () as PERCENTAGE
-from PREFECTURE as p
-inner join DRINK_HABITS as dh
-  on dh.PF_CODE = p.PF_CODE
-order by  ,p.PF_CODE DESC;
+select dh.PF_CODE as CODE, dh.PF_NAME as NAME, ROUND(100.0 * SUM(CASE dh.CATEGORY_CODE WHEN '120' THEN dh.AMT_CODE ELSE 0 END) / SUM(CASE dh.CATEGORY_CODE WHEN '110' THEN dh.AMT ELSE 0 END) , 1) as PERCENTAGE
+from DRINK_HABITS as dh
+inner join PREFECTURE as pf
+  on pf.PF_CODE = dh.PF_CODE
+where dh.GENDER_CODE IN('2', '3')
+group by dh.PF_CODE
+order by  PERCENTAGE DESC, p.PF_CODE DESC;
